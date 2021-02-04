3718038d9cc01b6a65da5137111b2492bfcd0e77
Felix Gliesche <felix@gliesche.net>
2021-02-04 16:28:57 +0100

    KOC-1603: add sample viewports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,44 @@
 
+const customViewports = {
+  mobile1: {
+    name: 'Small mobile',
+    styles: {
+      height: '568px',
+      width: '320px',
+    },
+    type: 'mobile',
+  },
+  mobile2: {
+    name: 'Large mobile',
+    styles: {
+      height: '896px',
+      width: '580px',
+    },
+    type: 'mobile',
+  },
+  tablet: {
+    name: 'Tablet',
+    styles: {
+      height: '1112px',
+      width: '834px',
+    },
+    type: 'tablet',
+  },
+  desktop: {
+    name: 'Desktop',
+    styles: {
+      height: '800px',
+      width: '1280px',
+    },
+    type: 'desktop',
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewport: {
+    viewports: customViewports,
+  },
 }
+
+
