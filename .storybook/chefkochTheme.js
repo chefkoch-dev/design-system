b8ab27b0aa9c017d0125c09682327e76d30b4587
Felix Gliesche <felix@gliesche.net>
2021-02-17 11:31:53 +0100

    KOC-1603: Add custom theme

---
+++ .storybook/chefkochTheme.js
@@ -0,0 +1,9 @@
+
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Chefkoch Design System',
+  brandUrl: 'https://chefkoch.de',
+  brandImage: 'https://design.chefkoch.de/logo.svg',
+});
