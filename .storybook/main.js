6cf8f9f113220dff2e0bef4e506006e1cdccc6ea
Felix Gliesche <felix@gliesche.net>
2021-02-17 11:30:54 +0100

    KOC-1603: Setup storybook to work with dsy vars

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,22 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-scss"
-  ]
+    "@storybook/preset-scss",
+  ],
+  webpackFinal: async config => {
+    // //Use loaders to handle scss
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'sass-resources-loader',
+          options: {
+            resources: ['./components/globals.scss'],
+          },
+        },
+      ],
+    })
+
+    return config;
+  },
 }
