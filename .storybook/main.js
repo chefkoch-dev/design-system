ada9525543439b6ac97063c542801198d411633a
Felix Gliesche <felix@gliesche.net>
2021-02-17 11:31:34 +0100

    KOC-1603: Add a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-scss",
+    '@storybook/addon-a11y',
   ],
   webpackFinal: async config => {
     // //Use loaders to handle scss
