20ceb38c21e410bc4be62ba63116286bc2a9403c
Felix Gliesche <felix@gliesche.net>
2021-03-03 18:29:47 +0100

    KOC-1603: Document buttons within component folder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,9 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../app/components/**/*.stories.mdx",
+    "../app/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
