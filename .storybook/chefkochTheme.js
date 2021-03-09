56457ce647c31d34d63adc8464848539ac9052b0
Felix Gliesche <felix@gliesche.net>
2021-03-09 13:32:21 +0100

    KOC-1603: Replace storybook logo

--- .storybook/chefkochTheme.js
+++ .storybook/chefkochTheme.js
@@ -1,10 +1,11 @@
 
 import { create } from '@storybook/theming';
 import { version } from "../package.json";
+import logo from "../stories/assets/logo.png";
 
 export default create({
   base: 'light',
   brandTitle: `Chefkoch Design System ${version}`,
   brandUrl: 'https://chefkoch.de',
-  brandImage: 'https://design.chefkoch.de/logo.svg',
+  brandImage: logo,
 });
